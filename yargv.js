1345ff7b6856f92bbbd4f6395c6d336e2fb6f7b2
Madhav1111 <98392912+Madhav1111@users.noreply.github.com>
2024-04-02 14:03:31 +0530

    Add files via upload

---
+++ yargv.js
@@ -0,0 +1,7 @@
+const a = require('yargs');
+const input = a.argv;
+console.log("the user input is :", input);
+console.log("the user input is :", input._[0]);
+console.log("the user input is :", input._[1]);
+
+
